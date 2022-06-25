199c9134439727ef814aa223e82bb6ace14a014c
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2022-06-25 19:57:45 +0300

    create feed component

--- packages/client/src/modules/home/Home.tsx
+++ packages/client/src/modules/home/Home.tsx
@@ -1,9 +1,11 @@
+import Feed from "components/feed";
 import Sidebar from "components/sidebar";
 
 const HomeModule = () => {
   return (
     <main>
       <Sidebar />
+      <Feed />
     </main>
   );
 };
